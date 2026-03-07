517078fdfc0b1ac215650ee7221d3dd71313082e
tunlinphyo <uniqueof1990@gmail.com>
2026-03-08 00:25:31 +0900

    Update UI

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,14 @@
 import './polyfills/dialog'
 import './polyfills/hover'
 import './style.css'
+
+document.addEventListener('DOMContentLoaded', inertFadeButton)
+
+function inertFadeButton() {
+  const hasCursorPointer = window.matchMedia('(hover: hover) and (pointer: fine)').matches
+
+  if (hasCursorPointer) {
+    const fadeButton = document.getElementById('fadeButton') as HTMLButtonElement
+    if (fadeButton) fadeButton.inert = true
+  }
+}
